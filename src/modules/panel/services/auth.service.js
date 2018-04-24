0d7d01aa3275296824a48cdb27b02beabf514347
Mehdi JorJor Zadeh <mehdijorjor@live.com>
2018-04-24 17:26:44 +0430

    d

--- src/modules/panel/services/auth.service.js
+++ src/modules/panel/services/auth.service.js
@@ -7,9 +7,11 @@ import messages from 'src/constants/defaults/messages.default'
 export default {
   async authenticateOperator({ username, password }) {
     const operator = await Operator.findOne({
-      username,
-      password,
-      status: statuses.OPERATOR.ACTIVE,
+      where: {
+        username,
+        password,
+        status: statuses.OPERATOR.ACTIVE,
+      }
     })
 
     if (!operator) throw new Error(messages.OPERATOR_NOT_FOUND)
